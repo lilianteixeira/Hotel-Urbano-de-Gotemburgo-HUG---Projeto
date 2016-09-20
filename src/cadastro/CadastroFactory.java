05bda4d02c55e71f66423b879cb438984b671ee7
Laerson Verissimo <laerson.verissimo@ccc.ufcg.edu.br>
2016-09-20 04:29:25 -0300

    Terminei de refatorar tudo do pacote cadastro

--- src/cadastro/CadastroFactory.java
+++ src/cadastro/CadastroFactory.java
@@ -3,16 +3,37 @@ package cadastro;
 import exceptions.StringInvalidaException;
 
 /**
- * O enum Cadastro Factory e responsavel pela criacao de um cadastro.
+ * Esse <code>Enum</code> tem como objetivo, fornecer um método público para
+ * instanciar objetos da classe <code>Cadastro</code>.
  * 
+ * @author laersonsv
+ * @see Cadastro
+ *
  */
 public enum CadastroFactory {
+	/*
+	 * Uso do enum para implementar o padrão singleton, é a melhor maneira, de
+	 * acordo com Joshua Bloch, no livro "Effective Java".
+	 */
 	INSTANCE;
 
 	/**
-	 * O metodo create retorna um novo cadastro
+	 * Retorna uma nova instância de <code>Cadastro</code>, que tem como
+	 * atributos os argumentos passados para este método.
+	 * <p>
+	 * Os argumentos são <code>Strings</code> que não podem guardar referência
+	 * null, ou não terem nenhum caractere gráfico.
 	 * 
-	 * @throws StringInvalidaException
+	 * @param nome
+	 *            nome a ser armazenado no cadastro
+	 * @param email
+	 *            email a ser armazenado no cadastro
+	 * @param dataDeNascimento
+	 *            data de nascimento a ser armazenada no cadastro
+	 * @return Uma nova instância de <code>Cadastro</code>, que tem como
+	 *         atributos os argumentos passados para este método.
+	 * @throws NullPointerException se algum dos argumentos tiver referência null.
+	 * @throws IllegalArgumentException se algum dos argumentos for composto só por espaço em brancos
 	 */
 	public Cadastro create(String nome, String email, String dataDeNascimento) throws StringInvalidaException {
 		return Cadastro.novoCadastro(nome, email, dataDeNascimento);
